4178250c083fa46ff50432b980586e2a82e0cdcf
unknown <mokaldeepak008@gmail,com>
2020-02-07 22:51:34 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const Datastore = require('nedb')
+const app = express();
+const fetch = require('node-fetch');
+require('dotenv').config()
+const port = process.env.PORT || 3000;
+const timestamp = Date.now();
+const database = new Datastore('darabase.db');
+
+app.use(express.static('public'));
+app.use(express.json({limit:'1mb'}));
+app.listen(port, ()=>{console.log(`api server is runnning on ${port}`)});
+database.loadDatabase();
+
+app.get('/api',(request, response)=>{
+    database.find({}, (error, data)=>{
+        if(error){
+            response.end();
+            return;
+        }
+        response.json(data);
+    })
+});
+
+
+app.post('/api', (request, response)=>{
+    const data = request.body;
+    data.timestamp = timestamp;
+    
+    database.insert(data);
+    
+    // response.json({
+    //     'status': 'success',
+    //     'latitude': data.latitude,
+    //     'longitude': data.longitude,
+    //     'timestamp': timestamp 
+    // });
+});
+
+app.get('/weather/:latlon', async(request,response)=>{
+    const latlon = request.params.latlon.split(',');
+    const lat = latlon[0];
+    const lon = latlon[1];
+    const api_key = process.env.API_KEY;
+    const weather_url = `https://api.darksky.net/forecast/${api_key}/${lat},${lon}`;
+    const weather_fetch = await fetch(weather_url);
+    const weather_data = await weather_fetch.json();
+
+    
+    const openaq_url = `https://api.openaq.org/v1/latest?coordinates=${lat},${lon}`;
+    const openaq_fetch = await fetch(openaq_url);
+    const openaq_data = await openaq_fetch.json();
+    
+    const data = {
+        weather: weather_data,
+        openaq: openaq_data
+    }
+    response.json(data);
+    
+})
+
+
---
+++ public/logs.js
@@ -0,0 +1,41 @@
+const mymap = L.map('checkinMap').setView([0, 0], 1);
+const attribution =
+  '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+  const tileUrl = 'https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png';
+  const tiles = L.tileLayer(tileUrl, { attribution });
+tiles.addTo(mymap);
+getData();
+
+async function getData(){
+    const response = await fetch('/api');
+    const data = await response.json();
+
+    for(let item of data){
+       
+        
+        try{
+           
+            const marker = L.marker([item.latitude, item.longitude]).addTo(mymap);
+            let txt = ` The weather informartion if 
+
+            <strong>City:</strong> ${item.weather_time}  ${item.weather.icon}
+            Temprature: ${item.weather.temperature} &deg;F 
+            Atmosphere: ${item.weather.summary}
+            Value:${item.air.value}
+            Unit: ${item.air.unit}
+            Parameters: ${item.air.parameter} 
+            Windspeed: ${item.weather.windSpeed}    
+            Dew point: ${item.weather.dewPoint}
+            Humidity: ${item.weather.humidity}
+            Last updated: ${item.air.lastUpdated }`
+        
+        marker.bindPopup(txt);
+    
+            
+        }catch(error){
+            console.error(error)
+        }
+    }
+   
+}
+
